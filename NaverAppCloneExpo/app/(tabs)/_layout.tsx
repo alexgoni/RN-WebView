221ee12077a0487f316f228eeb9d6c3f755107b6
alexgoni <goni000211@gmail.com>
2024-12-14 19:48:42 +0900

    feat: 네이버 홈 화면 웹뷰로 띄우기

--- NaverAppCloneExpo/app/(tabs)/_layout.tsx
+++ NaverAppCloneExpo/app/(tabs)/_layout.tsx
@@ -1,10 +1,42 @@
 import { Tabs } from "expo-router";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="shopping" />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "black" },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "홈",
+          tabBarIcon: HomeIcon,
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="shopping"
+        options={{
+          tabBarLabel: "쇼핑",
+          tabBarIcon: ShoppingIcon,
+          headerShown: false,
+        }}
+      />
     </Tabs>
   );
+}
+
+function HomeIcon({ focused, color }: { focused: boolean; color: string }) {
+  const iconName = focused ? "home" : "home-outline";
+
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
+}
+
+function ShoppingIcon({ focused, color }: { focused: boolean; color: string }) {
+  const iconName = focused ? "shopping" : "shopping-outline";
+
+  return <MaterialCommunityIcons name={iconName} color={color} size={26} />;
 }
--- NaverAppCloneExpo/app/(tabs)/index.tsx
+++ NaverAppCloneExpo/app/(tabs)/index.tsx
@@ -1,5 +1,39 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Platform, SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import WebView from "react-native-webview";
+
+const styles = StyleSheet.create({
+  safearea: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+});
 
 export default function HomeScreen() {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={styles.safearea}>
+      <WebView
+        source={{ uri: "https://m.naver.com/" }}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        onShouldStartLoadWithRequest={request => {
+          if (
+            request.url.startsWith("https://m.naver.com/") ||
+            request.mainDocumentURL?.startsWith("https://m.naver.com/")
+          ) {
+            return true;
+          }
+
+          if (request.url !== null && request.url.startsWith("https://")) {
+            router.navigate({
+              pathname: "browser",
+            });
+            return false;
+          }
+
+          return true;
+        }}
+      />
+    </SafeAreaView>
+  );
 }
